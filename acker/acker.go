3cfb9a7ca8e374532ec04dc65258e03e91043b4b
uhu99 <uhu99@t-online.de>
2013-08-29 22:24:19 +0200

    initial comit

---
+++ acker/acker.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func acker(n, m int32) int32 {
+	if n == 0 {
+		return m + 1
+	} else if m == 0 {
+		return acker(n-1, 1)
+	} else {
+		return acker(n-1, acker(n, m-1))
+	}
+}
+
+func main() {
+	n,e := arg(1)
+	m,f := arg(2)
+	if e != nil || f != nil {
+		os.Exit(1)
+	}
+	
+	a := acker(n, m)
+	fmt.Printf("acker(%d,%d) = %d\n", n, m, a)
+	fmt.Printf("%#v\n", os.Args)
+}
+
+func arg(i int) (int32, error) {
+	n,e := strconv.ParseInt(os.Args[i], 0, 32)
+	if e != nil {
+		fmt.Fprintln(os.Stderr, e.Error())
+		fmt.Fprintln(os.Stderr, "Nix verstehen: ", os.Args[i])
+	}
+
+	return int32(n), e
+}
+
---
+++ eins/eins.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func Sqrt(x float64) float64 {
+    z := 1.0
+    for i:=0; i<10; i++ {
+        z = z - (z*z - x) / (z+z)
+    }
+    return z
+}
+
+func main() {
+    fmt.Println(Sqrt(2))
+    fmt.Println(math.Sqrt(2))
+}
+
